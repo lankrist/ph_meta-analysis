3f1e787499a47c99db4d3cf9793dcf9f1189b7d6
lankrist <lankrist@umich.edu>
2015-06-04 14:55:10 -0400

    simulation

--- simulation.R
+++ simulation.R
@@ -1,7 +1,16 @@
 #meta-analysis 
 
 #simulation of data
+# 0: reference marker; 1: alternate marker
+mark <- setClass("mark", representation(type = "numeric"))
+ref <- new("mark", type = 0)
+alt <- new("mark", type = 1)
 
-#decision of weight function
- x <- c(3,2)
- x
+marker <- setClass("marker", representation(i = "mark", j = "mark"))
+
+
+marker(i = mark(0), j = mark(0))
+
+
+
+genome <- setClass("genome", slots = "matrix", )
