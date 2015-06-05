f921047c470434a9f6872a4fc735ca3b807a9db2
lankrist <lankrist@umich.edu>
2015-06-05 16:52:03 -0400

    simulation complete and cleaned

--- simulation.R
+++ simulation.R
@@ -1,6 +1,13 @@
-#meta-analysis 
+#meta-analysis simulation
 
-#function returns a simulated marker
+#VARIABLES
+samp_size = 100000
+al_freq = 0.2
+alpha = 2
+marker_types = c(0, 1, 2)
+
+#FUNCTION
+#Generates markers for a given sample size
 #takes in number of samples and allele frequency
 genegen <- function(samples, af){
   hap1=rbinom(samples,1,af)
@@ -9,40 +16,36 @@ genegen <- function(samples, af){
   return(gene)
 }
 
-samp_size = 100000
-al_freq = 0.2
-sims <- genegen(samp_size, al_freq)
-
-#generate allele proportions and odds ratio
-sim_prop <- table(sims)/samp_size
-names(sim_prop)
-sim_prop["1"]
-odds <- sim_prop/sim_prop["0"]
-
-alph = 2
-
-#funcition that generates probability of a phenotype to occur in a certain marker
-#takes in aray of marker samples, alph, beta
+#Generates probability of a phenotype to occur in a certain marker
+#takes in array of marker samples, alph, beta
 phenprob <- function(alpha, beta, marker){
   return(exp(alpha+beta*marker)/(1+exp(alpha+beta*marker)))
 }
 
-#generate conditional probability of illness given marker
-marker_types = c(0, 1, 2)
-logit_prob <- phenprob(alph, odds, marker_types)
+#SIMULATION
+#simulate genotypes
+sims <- genegen(samp_size, al_freq)
 
-#create phenotype probability
+#calculate allele proportions and odds ratio
+sim_prop <- table(sims)/samp_size
+odds <- sim_prop/sim_prop["0"]
+
+#calculate conditional probability of illness given marker
+logit_prob <- phenprob(alpha, odds, marker_types)
+
+#simulate phenotypes
 sim_phen <- c(
 rbinom(length(which(sims ==0)), 1, logit_prob[1]),
 rbinom(length(which(sims ==1)), 1, logit_prob[2]),
 rbinom(length(which(sims ==2)), 1, logit_prob[3]))
 
+#sort genotype data simulation
 sim_geno <- sort(sims)
 
+#compare data and convert to dataframe
 table(sim_phen, sim_geno)/samp_size
 geno <- data.frame(sim_geno, sim_phen)
 
 #output data
-write.table(geno, file = "/Users/kristine/Documents/Summer_2015/genomics/meta_analysis/ph_meta-analysis/sim_gene1.csv", sep=",")
-
+write.table(geno, file = "/Users/kristine/Documents/Summer_2015/genomics/meta_analysis/ph_meta-analysis/sim_gene4.csv", row.names = F, sep=",")
 
