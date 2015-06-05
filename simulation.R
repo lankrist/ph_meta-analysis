27837320f28192f17c929ef74339d30fd66d0418
lankrist <lankrist@umich.edu>
2015-06-05 16:33:45 -0400

    simulation complete

--- simulation.R
+++ simulation.R
@@ -10,8 +10,8 @@ genegen <- function(samples, af){
 }
 
 samp_size = 100000
-prop = 0.2
-sims <- genegen(samp_size, prop)
+al_freq = 0.2
+sims <- genegen(samp_size, al_freq)
 
 #generate allele proportions and odds ratio
 sim_prop <- table(sims)/samp_size
@@ -19,7 +19,7 @@ names(sim_prop)
 sim_prop["1"]
 odds <- sim_prop/sim_prop["0"]
 
-alph = 0.00005
+alph = 2
 
 #funcition that generates probability of a phenotype to occur in a certain marker
 #takes in aray of marker samples, alph, beta
@@ -31,13 +31,18 @@ phenprob <- function(alpha, beta, marker){
 marker_types = c(0, 1, 2)
 logit_prob <- phenprob(alph, odds, marker_types)
 
+#create phenotype probability
 sim_phen <- c(
-rbinom(table(sims)[1], 1, logit_prob[1]),
-rbinom(table(sims)[2], 1, logit_prob[2]),
-rbinom(table(sims)[3], 1, logit_prob[3]))
+rbinom(length(which(sims ==0)), 1, logit_prob[1]),
+rbinom(length(which(sims ==1)), 1, logit_prob[2]),
+rbinom(length(which(sims ==2)), 1, logit_prob[3]))
 
 sim_geno <- sort(sims)
 
 table(sim_phen, sim_geno)/samp_size
+geno <- data.frame(sim_geno, sim_phen)
+
+#output data
+write.table(geno, file = "/Users/kristine/Documents/Summer_2015/genomics/meta_analysis/ph_meta-analysis/sim_gene1.csv", sep=",")
 
 
