7b3b284058dccdaef02227a52bef6f01bc69509f
Rushil Sheth <rsheth@0587380156.wireless.umich.net>
2015-06-09 13:30:33 -0400

    correct code for geno_prob

--- simulation_v2.R
+++ simulation_v2.R
@@ -9,7 +9,7 @@ casamp <- 1000
 #FUNCTION
 #Returns genotype proportions
 #control_num = number of non-infected( = 0)
-#r is risk for allele; p is ...
+#r is risk for allele; p is allele frequency 
 geno_prob <- function(control, case, p, r){
   #d is prevalance of disease
   d = case/(control+case)
@@ -25,24 +25,31 @@ geno_prob <- function(control, case, p, r){
   prob_d2 = ((q^2)*d*r^2)/prob_disease
   
   #proportions of disease
-  disease <- do(case)*sample(c(0,1,2), 1, prob=c(prob_d0, prob_d1, prob_d2))
+  #disease <- do(case)*sample(c(0,1,2), 1, prob=c(prob_d0, prob_d1, prob_d2))
   
-  prob_healthy = (p^2)*(1-d)+2*p*q*((1-d)*r)+(q^2)*((1-d)*r^2)
+  prob_healthy = (p^2)*(1-d)+2*p*q*(1-d*r)+(q^2)*(1-d*r^2)
   
   #prob_h0 is prob that genotype is 0 given that diseased
   prob_h0 = ((p^2)*(1-d))/prob_healthy
   #prob_h1 is prob that genotype is 1 given that diseased
-  prob_h1 = (2*p*q*((1-d)*r))/prob_healthy
+  prob_h1 = (2*p*q*(1-d*r))/prob_healthy
   #prob_h2 is prob that genotype is 2 given that diseased
-  prob_h2 = ((q^2)*((1-d)*r^2))/prob_healthy
+  prob_h2 = ((q^2)*(1-d*r^2))/prob_healthy
   
+   
+  geno = c(case*prob_d0 + control*prob_h0, case*prob_d1 + control*prob_h1, case*prob_d2+control*prob_h2)
+  
+  return(geno)
   #proportions of healthy
-  healthy <- do(control)*sample(c(0,1,2), 1, prob=c(prob_h0, prob_h1, prob_h2))
+  #healthy <- do(control)*sample(c(0,1,2), 1, prob=c(prob_h0, prob_h1, prob_h2))
   
-  genotype = mapply(c, disease, healthy)  
+  #genotype = mapply(c, disease, healthy)  
 
-  return(genotype)  
+  #return(genotype)  
+
 }
+
+geno_prob(100,100,.5,.1)
 
 #SIMULATION
 #this portion should be written as function too
