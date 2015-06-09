170391704188cfd621afea019ccefcf1870ae9c9
lankrist <lankrist@umich.edu>
2015-06-08 21:12:20 -0400

    vector output

--- simulation_v2.R
+++ simulation_v2.R
@@ -1,10 +1,11 @@
-#meta-analysis simulation
+#meta-analysis simulation 2
+library(mosaic)
 
-
+#FUNCTION
+#Returns genotype proportions
 #control_num = number of non-infected( = 0)
-#r is risk for allele
-
-simul_geno <- function(control, case, p, r){
+#r is risk for allele; p is ...
+geno_prob <- function(control, case, p, r){
   #d is prevalance of disease
   d = case/(control+case)
   q = 1-p
@@ -18,6 +19,8 @@ simul_geno <- function(control, case, p, r){
   #prob_d2 is prob that genotype is 2 given that diseased
   prob_d2 = ((q^2)*d*r^2)/prob_disease
   
+  #proportions of disease
+  disease <- do(control)*sample(c(0,1,2), 1, prob=c(prob_d0, prob_d1, prob_d2))
   
   prob_healthy = (p^2)*(1-d)+2*p*q*((1-d)*r)+(q^2)*((1-d)*r^2)
   
@@ -28,9 +31,19 @@ simul_geno <- function(control, case, p, r){
   #prob_h2 is prob that genotype is 2 given that diseased
   prob_h2 = ((q^2)*((1-d)*r^2))/prob_healthy
   
-  genotype = c(prob_d0*case + prob_h0*control, prob_d1*case + prob_h1*control, prob_d2*case + prob_h2*control)  
+  #proportions of healthy
+  healthy <- do(control)*sample(c(0,1,2), 1, prob=c(prob_h0, prob_h1, prob_h2))
+  
+  genotype = mapply(c, disease, healthy)  
 
   return(genotype)  
 }
 
-simul_geno(100,100, .5, .1)
+#SIMULATION
+gprob <- geno_prob(100,100, .5, .1)
+pprob <- c(rep(1, 100), rep(0, 100))
+geno <- data.frame(gprob, pprob)
+write.table(geno, 
+            file = "/Users/kristine/Documents/Summer_2015/genomics/meta_analysis/ph_meta-analysis/sim_gene4.csv", 
+            row.names = F, sep=",")
+
