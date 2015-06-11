11d2aad5c1d0ccabe9b00b92dd4abcc543f64214
lankrist <lankrist@umich.edu>
2015-06-11 14:57:54 -0400

    add score test code

--- parameter_gen.R
+++ parameter_gen.R
@@ -1,18 +1,29 @@
 #generate parameters
+install.packages("statmod")
+Rlibrary(statmod)
 
 #load data
-sims <- read.csv("/Users/kristine/Documents/Summer_2015/genomics/meta_analysis/ph_meta-analysis/sim_gene1.csv", header = T)
+sims <- read.csv("/Users/kristine/Documents/Summer_2015/genomics/meta_analysis/ph_meta-analysis/sim_gene.csv", header = T)
 head(sims); table(sims)
 
 #LOGISTIC REGRESSION
-lreg <- glm(formula = sim_phen ~ sim_geno, family = binomial(logit), data = sims)
-names(lreg)
-summary(lreg)
+lreg <- glm(formula = sim_pheno ~ result, family = binomial(logit), data = sims)
+names(lreg);summary(lreg)
+
 #coefficients
 lreg_beta <- lreg$coefficients
 #std_err
-lreg_err <- mean(lreg$residuals)
-#deviance
-lreg_deviance <- lreg$deviance
+lreg_err <- summary(lreg)$coefficients[,2]
 
+#SCORE TEST
+score_test <- function(vector){
+  gen_mean <- mean(vector$result)
+  phen_mean <- mean(vector$sim_pheno)
+  return(crossprod((vector[,1] - mean(vector[,1])), (vector[,2] - mean(vector[,2]))))
+}
+
+score_test(sims)
+
+#sims <- data.frame(result = c(11, 15, 3, 7), sim_pheno = c(10, 12, 19, 4))
+#crossprod((sims[,1] - mean(sims[,1])), (sims[,2] - mean(sims[,2])))
 
