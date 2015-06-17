a369434d46d0ac33f540bd9043d08337e732576d
Rushil Sheth <rsheth@0587358319.wireless.umich.net>
2015-06-17 16:11:01 -0400

    fixed geno_prob

--- powerTests.R
+++ powerTests.R
@@ -1,5 +1,5 @@
 
-rgs <- commandArgs(TRUE)
+Args <- commandArgs(TRUE)
 if (length(Args)!=5){
   print ("Usage: CaseNo ControlNo Allele_Freq Risk_Ratio Rounds")
   quit()
@@ -11,6 +11,11 @@ Al_fr <- as.numeric(Args[3])
 Risk<-as.numeric( Args[4])
 Rounds <-as.numeric(Args[5])
 
+cosamp <- 1000
+casamp <- 1000
+al_fr <- 0.2
+risk <- 2
+rounds <- 100
 
 #cosamp, casamp, al_fr, risk, roundscosamp
 
@@ -31,19 +36,19 @@ geno_prob <- function(control, case, p, r){
   #prob_d2 is prob that genotype is 2 given that diseased
   prob_d2 = ((q^2)*d*r^2)/prob_disease
   #proportions of disease
-  disease <- sample(c(0,1,2), case, prob=c(prob_d0, prob_d1, prob_d2),replace=TRUE)
+  disease <- sample(c(0,1,2), case, replace=TRUE, prob=c(prob_d0, prob_d1, prob_d2))
   
-  prob_healthy = (p^2)*(1-d)+2*p*q*(1-d*r)+(q^2)*(1-d*r^2)
+  prob_healthy = (p^2)*(1-d)+(2*p*q*(1-d)*r)+((q^2)*(1-d)*r^2)
   
   #prob_h0 is prob that genotype is 0 given that diseased
   prob_h0 = ((p^2)*(1-d))/prob_healthy
   #prob_h1 is prob that genotype is 1 given that diseased
-  prob_h1 = (2*p*q*(1-d*r))/prob_healthy
+  prob_h1 = (2*p*q*(1-d)*r)/prob_healthy
   #prob_h2 is prob that genotype is 2 given that diseased
-  prob_h2 = ((q^2)*(1-d*r^2))/prob_healthy
+  prob_h2 = ((q^2)*(1-d)*r^2)/prob_healthy
   
   #proportions of healthy
-  healthy <- sample(c(0,1,2), control, prob=c(prob_h0, prob_h1, prob_h2),replace=TRUE)
+  healthy <- sample(c(0,1,2), control,replace=TRUE, prob=c(prob_h0, prob_h1, prob_h2))
   
   #geno = c(case*prob_d0 + control*prob_h0, case*prob_d1 + control*prob_h1, case*prob_d2+control*prob_h2)
   #return(geno)
@@ -81,7 +86,7 @@ score_test <- function(vector){
   score <- crossprod( (vector[,1] - mean(vector[,1])), (vector[,2] - mean(vector[,2])) )
   StdErr <- sqrt( crossprod( (vector[,2] - mean(vector[,2]))^2, (vector[,1] - mean(vector[,1]))^2 ) )
   p = 2*pnorm(-abs(score/StdErr))
-  return(c(score/StdErr, sqrt(StdErr), p))
+  return(c(score/StdErr, StdErr, p))
 }
 
 #ALLELE FREQUENCY TEST
@@ -131,8 +136,8 @@ powerReturn <- function(cosamp, casamp, al_fr, risk, rounds){
   pValChi=NULL
   pValProp=NULL
   
-  
   for (i in 1:rounds) {
+    
     data=simulation(cosamp, casamp, al_fr, risk)
     
     temp = log_reg(data)
@@ -146,11 +151,13 @@ powerReturn <- function(cosamp, casamp, al_fr, risk, rounds){
     
     temp = chi_sq(data)
     pValChi=c(pValChi,PValInd(temp[1]))
+    
   }
+  
   
   return(c(mean(pValLog), mean(pValScore), mean(pValProp), mean(pValChi)))
 }
 
 
-print(powerReturn(Cosamp,Casamp,Al_fr,Risk,Rounds))
+print(powerReturn(cosamp,casamp,al_fr,risk,rounds))
                                       
