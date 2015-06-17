ef2a8218b59f2776bdf1e705fb99cbf4d0d2c35b
lankrist <lankrist@umich.edu>
2015-06-17 16:13:25 -0400

    powerTests check

--- powerTests.R
+++ powerTests.R
@@ -1,5 +1,6 @@
+#compiled version of Power Tests
 
-rgs <- commandArgs(TRUE)
+Args <- commandArgs(TRUE)
 if (length(Args)!=5){
   print ("Usage: CaseNo ControlNo Allele_Freq Risk_Ratio Rounds")
   quit()
@@ -8,11 +9,9 @@ if (length(Args)!=5){
 Cosamp=as.numeric(Args[1])
 Casamp=as.numeric(Args[2])
 Al_fr <- as.numeric(Args[3])
-Risk<-as.numeric( Args[4])
+Risk<-as.numeric(Args[4])
 Rounds <-as.numeric(Args[5])
-
-
-#cosamp, casamp, al_fr, risk, roundscosamp
+#cosamp, casamp, al_fr, risk, rounds
 
 #FUNCTION
 #Returns genotype proportions
@@ -34,32 +33,25 @@ geno_prob <- function(control, case, p, r){
   disease <- sample(c(0,1,2), case, prob=c(prob_d0, prob_d1, prob_d2),replace=TRUE)
   
   prob_healthy = (p^2)*(1-d)+2*p*q*(1-d*r)+(q^2)*(1-d*r^2)
-  
   #prob_h0 is prob that genotype is 0 given that diseased
   prob_h0 = ((p^2)*(1-d))/prob_healthy
   #prob_h1 is prob that genotype is 1 given that diseased
   prob_h1 = (2*p*q*(1-d*r))/prob_healthy
   #prob_h2 is prob that genotype is 2 given that diseased
   prob_h2 = ((q^2)*(1-d*r^2))/prob_healthy
-  
   #proportions of healthy
   healthy <- sample(c(0,1,2), control, prob=c(prob_h0, prob_h1, prob_h2),replace=TRUE)
   
-  #geno = c(case*prob_d0 + control*prob_h0, case*prob_d1 + control*prob_h1, case*prob_d2+control*prob_h2)
-  #return(geno)
-  genotype = c( disease, healthy)
+  genotype = c(disease, healthy)
   return(genotype)
-  
 }
+
 #put all the simulations together
 simulation <- function(control_samp, case_samp, dom_allele, risk_factor){
   gprob <- geno_prob(control_samp,case_samp, dom_allele, risk_factor)
-  cosamp=control_samp
-  casamp=case_samp
-  pprob <- c(rep(1, cosamp), rep(0, casamp))
+  pprob <- c(rep(1, control_samp), rep(0, case_samp))
   return(data.frame(sim_geno = gprob, sim_pheno = pprob))
 }
-
 
 
 #generate parameters
@@ -67,27 +59,28 @@ simulation <- function(control_samp, case_samp, dom_allele, risk_factor){
 #returns log_reg beta_0, beta_1, log_reg error
 log_reg <- function(v){
   lreg <- glm(formula = v[,2] ~ v[,1], family = binomial(logit), data = v)
-  #coefficients
+  #beta_1
   lreg_beta <- summary(lreg)$coefficients[,1][2]
   #std_err
   lreg_err <- summary(lreg)$coefficients[,2][2]
+  #pval
   pVal <- summary(lreg)$coefficients[,4][2]
   return(c(lreg_beta, lreg_err,pVal))
 }
 
 #SCORE TEST
-#returns z-score, standard error, p val
+#returns standard error, z-score, p val
 score_test <- function(vector){
   score <- crossprod( (vector[,1] - mean(vector[,1])), (vector[,2] - mean(vector[,2])) )
   StdErr <- sqrt( crossprod( (vector[,2] - mean(vector[,2]))^2, (vector[,1] - mean(vector[,1]))^2 ) )
   p = 2*pnorm(-abs(score/StdErr))
-  return(c(score/StdErr, sqrt(StdErr), p))
+  return(c(StdErr, score/StdErr, p))
 }
 
 #ALLELE FREQUENCY TEST
 #risk allele is q
 #input vector that is 2 x n where n is the number of samples(controls+cases)
-#returns standard error, p-value, z score
+#returns standard error, z score, p value
 allele_freq <- function(dat){
   
   Case=which(dat[,2]==1)
@@ -99,11 +92,9 @@ allele_freq <- function(dat){
   PCtrl=sum(dat[Ctrl,1])/(2*NCtrl)
   
   stand_error = sqrt(  (PCase*(1-PCase))/(2*NCase) + (PCtrl*(1-PCtrl))/(2*NCtrl) )
-  
   z = (PCase - PCtrl)/stand_error
   p = 2*pnorm(-abs(z))
-  result = c(stand_error, z,p)
-  return(result)
+  return(c(stand_error, z, p))
 }
 
 #CHI SQUARE TEST
@@ -112,20 +103,13 @@ chi_sq <- function(v){
   return(chisq.test(table(v))$p.value)
 }
 
-PValInd = function(x)
-{
-  if(x<0.05)
-  {
-    return(1)
-  }
-  else
-  {
-    return(0)
-  }
+#checks pvalue significance
+PValInd = function(x){
+  if(x<0.05){return(1)}
+  else {return(0)}
 }
 
 powerReturn <- function(cosamp, casamp, al_fr, risk, rounds){
-  
   pValLog=NULL
   pValScore=NULL
   pValChi=NULL
--- riskValRuns.R
+++ riskValRuns.R
@@ -1,12 +1,11 @@
 riskVal <- NULL
-for (i in -3:3) {
-  riskVal <- c(risk, 2**i)
+for (i in -3:0) {
+  riskVal <- c(riskVal, 2**i)
 }
 
 for(risk in riskVal)
 {
-  
-  system(paste("cat NewPowerGenerator.r | R --slave --no-save --args 2000 2000 0.2 ",risk," 10000 > TempOutput_for_Risk_Factor_",risk ,
+  system(paste("cat powerTests.r | R --slave --no-save --args 2000 200 0.2 ",risk," 1000 > TempOutput_for_Risk_Factor_",risk ,
                " &",sep=""))
-  
+ 
 }
