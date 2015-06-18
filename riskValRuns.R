e9bb89908d66750d8c6fbc0a8afe2e7158ea5192
lankrist <lankrist@umich.edu>
2015-06-18 14:48:50 -0400

    Goncalo edit
    d

--- riskValRuns.R
+++ riskValRuns.R
@@ -1,11 +1,12 @@
-riskVal <- NULL
-for (i in -5:0) {
-  riskVal <- c(riskVal, 2**i)
-}
+#Goncala version riskvalRuns
+source("powerTests2.r")
 
-for(risk in riskVal)
-{
-  system(paste("cat powerTests.r | R --slave --no-save --args 1000 500 0.2 ",risk," 1000 > TempOutput_for_Risk_Factor_",risk ,
-               " &",sep=""))
- 
-}
+Casamp = 10000
+Cosamp = 10000
+rounds = 100
+
+cat("Ncases Ncontrols AF Risk Power_Log Power_Score Power_Prop Power_Chi\n")
+
+for (af in c(0.01, 0.05, 0.10, 0.20, 0.50))
+  for (risk in c(1.0, 1.5, 2.0, 5.0))
+    cat(Casamp, Cosamp, af, risk, powerReturn(Cosamp,Casamp,af,risk,rounds), "\n")
